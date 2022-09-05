10797ea8a3a55f518cce3cb17698480741305d46
Amr Guaily <amrguaily@gmail.com>
2022-09-05 22:48:28 +0200

    upload pin to firebase with another approach

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import { PrivateRoute } from './components/index';
-import { UploadProvider } from './context/UploadContext';
 import {
   Login,
   Signup,
@@ -18,19 +17,13 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
+        {/* Private Routes */}
         <Route path="/*" element={<PrivateRoute />}>
           <Route path="/*" element={<Home />} />
         </Route>
 
         <Route path="/create-pin" element={<PrivateRoute />}>
-          <Route
-            path=""
-            element={
-              <UploadProvider>
-                <CreatePin />
-              </UploadProvider>
-            }
-          />
+          <Route path="" element={<CreatePin />} />
         </Route>
 
         <Route path="/category/:categoryName" element={<PrivateRoute />}>
--- src/pages/CreatePin.jsx
+++ src/pages/CreatePin.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import { useUpload } from '../context/UploadContext';
+import { useNavigate } from 'react-router-dom';
 import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { GrFormClose } from 'react-icons/gr';
 import { categories } from '../assets/DummyData';
 import { UserImg } from '../components';
 import { useAuth } from '../context/AuthContext';
+import { addDoc, doc, serverTimestamp, updateDoc } from 'firebase/firestore';
+import { pinCollectionRef, storage } from '../lib/firebase';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 
 const CreatePin = () => {
   const [formData, setFormData] = useState({
@@ -11,29 +15,64 @@ const CreatePin = () => {
     about: '',
     category: '',
   });
-
-  const { uploadHandler, fileHandler, progress, url, alert, loading } =
-    useUpload();
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
   const { currentUser } = useAuth();
-  const { photoUrl, email, displayName } = currentUser.reloadUserInfo;
+  const navigate = useNavigate();
+  const { photoUrl, email, displayName, localId } = currentUser.reloadUserInfo;
 
-  function changeHandler(e) {
+  const fileHandler = (e) => {
+    const types = ['image/png', 'image/jpeg', 'image/webp', 'image/jfif'];
+    const reader = new FileReader();
+
+    const selectedFile = e.target.files[0];
+    if (types.includes(selectedFile.type)) {
+      reader.readAsDataURL(selectedFile);
+    }
+    reader.onload = (e) => {
+      setFile(e.target.result);
+    };
+  };
+
+  const changeHandler = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
-  }
+  };
 
-  function submitHandler(e) {
+  async function submitHandler(e) {
     e.preventDefault();
-    uploadHandler(formData);
+    if (!file) return;
+
+    setLoading(true);
+    const docRef = await addDoc(pinCollectionRef, {
+      createdAt: serverTimestamp(),
+      formData,
+      postedby: {
+        id: localId,
+        name: displayName ? displayName : email.split('@')[0],
+        photoUrl: photoUrl ? photoUrl : null,
+      },
+      saves: [],
+    });
+
+    const storageRef = ref(storage, `pins/${formData.title}`);
+    await uploadString(storageRef, file, 'data_url').then(async () => {
+      const downloadUrl = await getDownloadURL(storageRef);
+      await updateDoc(doc(pinCollectionRef, docRef.id), {
+        url: downloadUrl,
+      });
+    });
+
+    navigate('/', { replace: true });
   }
 
   return (
     <>
-      {alert && (
+      {/* {alert && (
         <div className="w-[80%] min-w-[300px] px-3 mx-auto my-4 font-semibold text-white h-10 flex items-center justify-center rounded-md bg-red-500 animate-show-in">
           <p>{alert}</p>
         </div>
-      )}
+      )} */}
 
       <form
         className="w-[90%] mt-10 rounded-md mx-auto flex flex-col laptop:flex-row justify-center items-center gap-4 bg-white p-5 relative"
@@ -41,26 +80,19 @@ const CreatePin = () => {
       >
         {/* Left Side */}
         <div className="bg-gray-200 w-full laptop:max-w-[380px] h-[340px] p-3 relative">
-          {/* progress par */}
-          <div
-            className=" absolute h-1 top-[-4px] left-0 bg-blue-500"
-            style={{ width: `${progress}%` }}
-          />
           {/* Image Preview */}
-          {url ? (
-            <img
-              src={url}
-              alt="uploaded-img"
-              className="h-full w-full object-contain mx-auto"
-            />
+          {file ? (
+            <div className="relative h-full w-full ">
+              <img src={file} alt="uploaded-img" className="h-full w-full" />
+              <GrFormClose
+                size={24}
+                className="absolute top-2 left-2 cursor-pointer bottom-1 rounded-full shadow-md bg-white/60 hover:bg-white/90"
+                onClick={() => setFile(null)}
+              />
+            </div>
           ) : (
             <label className="h-full flex items-center justify-center border-dotted border-2 border-gray-400 cursor-pointer">
-              <input
-                type="file"
-                className="w-0 h-0"
-                required
-                onChange={(e) => fileHandler(e.target.files[0])}
-              />
+              <input type="file" hidden onChange={fileHandler} />
               <div className="font-semibold">
                 <AiOutlineCloudUpload size={26} className="mx-auto" />
                 <p>Click To Upload</p>
@@ -74,6 +106,7 @@ const CreatePin = () => {
           <input
             type="text"
             name="title"
+            required
             value={formData.title}
             onChange={changeHandler}
             placeholder="Pin Title"
@@ -101,8 +134,8 @@ const CreatePin = () => {
             <h3 className=" font-semibold text-xl mb-3">Choose Pin Category</h3>
             <select
               name="category"
-              required
               value={formData.category}
+              required
               onChange={changeHandler}
               className="w-[80%] p-1.5 border-b-3 outline-none text-lg tablet:text-xl border-b-2 border-gray-200"
             >
@@ -117,8 +150,8 @@ const CreatePin = () => {
         </div>
 
         <button
-          disabled={loading}
-          className="self-end laptop:absolute bottom-4 right-4 py-1 px-3 bg-red-500 cursor-pointer text-white font-semibold rounded-xl"
+          disabled={!file}
+          className="self-end laptop:absolute bottom-4 right-4 py-1 px-3 bg-red-500 text-white font-semibold rounded-xl disabled:bg-opacity-70"
         >
           {!loading ? 'Submit' : 'Processing...'}
         </button>
